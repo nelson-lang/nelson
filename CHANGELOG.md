bfae1f7dc1f84f45bbfab22dc840d37990e89db9
Allan CORNET <nelson.numerical.computation@gmail.com>
2017-01-16 22:05:54 +0100

    add ceil, floor, fix, round functions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 Features:
 ---------
 
+  Add ceil, floor, round, fix functions.
+
   Add sparse support for acos, asin, atan, cos, cosh, sin, sinh, tan, tanh.
 
   Add website and bug reports link in help menu.
--- modules/elementary_functions/builtin/cpp/Gateway.cpp
+++ modules/elementary_functions/builtin/cpp/Gateway.cpp
@@ -57,6 +57,10 @@
 #include "numelBuiltin.hpp"
 #include "isapproxBuiltin.hpp"
 #include "uplusBuiltin.hpp"
+#include "ceilBuiltin.hpp"
+#include "floorBuiltin.hpp"
+#include "roundBuiltin.hpp"
+#include "fixBuiltin.hpp"
 //=============================================================================
 using namespace Nelson;
 //=============================================================================
@@ -106,6 +110,10 @@ static const nlsGateway gateway[] =
     { "isequaln", Nelson::ElementaryFunctionsGateway::isequalnBuiltin, 1, -1 },
     { "numel", Nelson::ElementaryFunctionsGateway::numelBuiltin, 1, 1 },
     { "isapprox", Nelson::ElementaryFunctionsGateway::isapproxBuiltin, 1, -2},
+	{ "ceil", Nelson::ElementaryFunctionsGateway::ceilBuiltin, 1, 1 },
+	{ "floor", Nelson::ElementaryFunctionsGateway::floorBuiltin, 1, 1 },
+	{ "fix", Nelson::ElementaryFunctionsGateway::fixBuiltin, 1, 1 },
+	{ "round", Nelson::ElementaryFunctionsGateway::roundBuiltin, 1, 1 },
 };
 //=============================================================================
 NLSGATEWAYFUNC(gateway)
