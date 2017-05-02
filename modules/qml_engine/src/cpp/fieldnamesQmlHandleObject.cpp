a172ee3ad6ca322600cd232a534713e36a72645a
Allan CORNET <nelson.numerical.computation@gmail.com>
2017-05-02 21:18:12 +0200

    fix build linux

--- modules/qml_engine/src/cpp/fieldnamesQmlHandleObject.cpp
+++ modules/qml_engine/src/cpp/fieldnamesQmlHandleObject.cpp
@@ -16,8 +16,6 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // LICENCE_BLOCK_END
 //=============================================================================
-#pragma once
-//=============================================================================
 #include <QtQml/QQmlComponent>
 #include "fieldnamesQmlHandleObject.hpp"
 #include "Exception.hpp"
