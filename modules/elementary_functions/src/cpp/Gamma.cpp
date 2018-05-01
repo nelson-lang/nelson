92a980ef488f7b2a66d5b27fcc7246b0e53727de
Allan CORNET <nelson.numerical.computation@gmail.com>
2018-05-01 21:35:56 +0200

    gamma function with tests

--- modules/elementary_functions/src/cpp/Gamma.cpp
+++ modules/elementary_functions/src/cpp/Gamma.cpp
@@ -16,54 +16,68 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // LICENCE_BLOCK_END
 //=============================================================================
+#include <functional>
+#include <boost/math/special_functions/gamma.hpp>
 #include <Eigen/Dense>
-#include <Eigen/Core>
-#include <unsupported/Eigen/SpecialFunctions>
 #include "Gamma.hpp"
 #include "Exception.hpp"
 #include "ClassName.hpp"
 //=============================================================================
 namespace Nelson {
     //=============================================================================
-	ArrayOf Gamma(ArrayOf arrayIn)
+    static double gammad(double v)
+    {
+        if ((v == 0) || std::isinf(v) || (v >= 172) || floor(v) == v)
+        {
+            return  std::numeric_limits<double>::infinity();
+        }
+        return boost::math::tgamma(v);
+    }
+    //=============================================================================
+    static single gammas(single v)
+    {
+        if ((v == 0) || std::isinf(v) || (v >= 36) || floor(v) == v)
+        {
+            return  std::numeric_limits<single>::infinity();
+        }
+        return boost::math::tgamma(v);
+    }
+    //=============================================================================
+    ArrayOf Gamma(ArrayOf arrayIn)
     {
         ArrayOf res;
-		if (arrayIn.isSparse() ||
-			arrayIn.getDataClass() == NLS_DCOMPLEX ||
-			arrayIn.getDataClass() == NLS_SCOMPLEX) 
-		{
-			throw Exception(_W("Input argument must be dense and real."));
-		}
-		if (arrayIn.getDataClass() == NLS_DOUBLE || arrayIn.getDataClass() == NLS_SINGLE)
-		{
-			Dimensions dimsIn = arrayIn.getDimensions();
-			res = arrayIn;
-			res.ensureSingleOwner();
-			if (arrayIn.getDataClass() == NLS_DOUBLE)
-			{
-				if (!arrayIn.isEmpty())
-				{
-					Eigen::Map<Eigen::ArrayXd> matIn((double*)arrayIn.getDataPointer(), dimsIn.getElementCount());
-					Eigen::Map<Eigen::ArrayXd> matOut((double*)res.getDataPointer(), dimsIn.getElementCount());
-					matOut = matIn.lgamma();
-					matOut = matOut.exp();
-				}
-			}
-			else
-			{
-				if (!arrayIn.isEmpty())
-				{
-					Eigen::Map<Eigen::ArrayXf> matIn((single*)arrayIn.getDataPointer(), dimsIn.getElementCount());
-					Eigen::Map<Eigen::ArrayXf> matOut((single*)res.getDataPointer(), dimsIn.getElementCount());
-					matOut = matIn.lgamma();
-					matOut = matOut.exp();
-				}
-			}
-		}
-		else
-		{
-			throw Exception(_("Undefined function 'gamma' for input arguments of type") + " '" + ClassName(arrayIn) + "'.");
-		}
+        if (arrayIn.isSparse() ||
+                arrayIn.getDataClass() == NLS_DCOMPLEX ||
+                arrayIn.getDataClass() == NLS_SCOMPLEX)
+        {
+            throw Exception(_W("Input argument must be dense and real."));
+        }
+        if (arrayIn.getDataClass() == NLS_DOUBLE || arrayIn.getDataClass() == NLS_SINGLE)
+        {
+            Dimensions dimsIn = arrayIn.getDimensions();
+            res = arrayIn;
+            res.ensureSingleOwner();
+            if (arrayIn.getDataClass() == NLS_DOUBLE)
+            {
+                if (!arrayIn.isEmpty())
+                {
+                    Eigen::Map<Eigen::ArrayXd> matOut((double*)res.getDataPointer(), dimsIn.getElementCount());
+                    matOut = matOut.unaryExpr(std::ptr_fun(gammad));
+                }
+            }
+            else
+            {
+                if (!arrayIn.isEmpty())
+                {
+                    Eigen::Map<Eigen::ArrayXf> matOut((single*)res.getDataPointer(), dimsIn.getElementCount());
+                    matOut = matOut.unaryExpr(std::ptr_fun(gammas));
+                }
+            }
+        }
+        else
+        {
+            throw Exception(_("Undefined function 'gamma' for input arguments of type") + " '" + ClassName(arrayIn) + "'.");
+        }
         return res;
     }
     //=============================================================================
