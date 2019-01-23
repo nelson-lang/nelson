716037642320c169a58f734178260d9b66fc0402
Allan CORNET <nelson.numerical.computation@gmail.com>
2019-01-23 19:52:32 +0100

    save single support

--- modules/hdf5/src/cpp/h5SaveSingle.cpp
+++ modules/hdf5/src/cpp/h5SaveSingle.cpp
@@ -17,14 +17,158 @@
 // LICENCE_BLOCK_END
 //=============================================================================
 #include "h5SaveSingle.hpp"
+#include "h5SaveHelpers.hpp"
+#include "h5SaveLoadHelpers.hpp"
+#include "Exception.hpp"
 //=============================================================================
 namespace Nelson {
+//=============================================================================
+static bool
+h5SaveSingleEmptyMatrix(
+    hid_t fid, const std::string& location, const std::string& variableName, ArrayOf VariableValue);
+//=============================================================================
+static bool
+h5SaveSingleMatrix(
+    hid_t fid, const std::string& location, const std::string& variableName, ArrayOf VariableValue);
 //=============================================================================
 bool
 h5SaveSingle(
     hid_t fid, const std::string& location, const std::string& variableName, ArrayOf VariableValue)
 {
     bool bSuccess = false;
+    if (VariableValue.isEmpty(false)) {
+        bSuccess = h5SaveSingleEmptyMatrix(fid, location, variableName, VariableValue);
+    } else {
+        bSuccess = h5SaveSingleMatrix(fid, location, variableName, VariableValue);
+    }
+    return bSuccess;
+}
+//=============================================================================
+bool
+h5SaveSingleEmptyMatrix(
+    hid_t fid, const std::string& location, const std::string& variableName, ArrayOf VariableValue)
+{
+    bool bSuccess = false;
+    std::string h5path = location + variableName;
+    herr_t status = H5Ldelete(fid, h5path.c_str(), H5P_DEFAULT);
+
+    single value = 0;
+    hid_t type_id = H5Tcopy(H5T_NATIVE_FLOAT);
+    hsize_t dimsAsHsize_t[1];
+    dimsAsHsize_t[0] = 1;
+    hid_t dspace_id = H5Screate_simple((int)1, dimsAsHsize_t, dimsAsHsize_t);
+    if (dspace_id < 0) {
+        return false;
+    }
+
+    hid_t dataset_id
+        = H5Dcreate(fid, h5path.c_str(), type_id, dspace_id, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    if (dataset_id < 0) {
+        return false;
+    }
+    status = H5Dwrite(dataset_id, type_id, H5S_ALL, H5S_ALL, H5P_DEFAULT, &value);
+
+    H5Dclose(dataset_id);
+    H5Sclose(dspace_id);
+    if (status < 0) {
+        return false;
+    }
+    bSuccess = h5SaveEmptyAttribute(fid, h5path);
+    if (!bSuccess) {
+        return false;
+    }
+    bSuccess = h5SaveClassAttribute(fid, h5path, VariableValue);
+    if (bSuccess) {
+        if (VariableValue.isComplex()) {
+            bSuccess = h5SaveComplexAttribute(fid, h5path);
+        }
+        if (bSuccess) {
+            bSuccess = h5SaveDimensionsAttribute(fid, h5path, VariableValue.getDimensions());
+        }
+    }
+    return bSuccess;
+}
+//=============================================================================
+bool
+h5SaveSingleMatrix(
+    hid_t fid, const std::string& location, const std::string& variableName, ArrayOf VariableValue)
+{
+    bool bSuccess = false;
+
+    std::string h5path = location + variableName;
+    herr_t status = H5Ldelete(fid, h5path.c_str(), H5P_DEFAULT);
+
+    hid_t dspace_id = H5I_INVALID_HID;
+    hid_t type_id = H5Tcopy(H5T_NATIVE_FLOAT);
+    Dimensions dimsValue = VariableValue.getDimensions();
+
+    hsize_t* dimsAsHsize_t = nullptr;
+    indexType nbElementsSizeData;
+    if (dimsValue.isScalar()) {
+        try {
+            dimsAsHsize_t = new_with_exception<hsize_t>(1, true);
+        } catch (Exception&) {
+            return false;
+        }
+        nbElementsSizeData = 1;
+        dimsAsHsize_t[0] = 1;
+        dspace_id = H5Screate_simple((int)1, dimsAsHsize_t, dimsAsHsize_t);
+    } else {
+        try {
+            dimsAsHsize_t = new_with_exception<hsize_t>(dimsValue.getLength(), true);
+        } catch (Exception&) {
+            return false;
+        }
+        nbElementsSizeData = dimsValue.getLength();
+        for (indexType k = 1; k <= nbElementsSizeData; k++) {
+            dimsAsHsize_t[k - 1] = (hsize_t)dimsValue[nbElementsSizeData - k];
+        }
+        dspace_id = H5Screate_simple((int)dimsValue.getLength(), dimsAsHsize_t, dimsAsHsize_t);
+    }
+    delete[] dimsAsHsize_t;
+
+    void* buffer = (void*)VariableValue.getDataPointer();
+    if (VariableValue.isComplex()) {
+        typedef struct complex_type
+        {
+            single r;
+            single i;
+        } complex_type;
+        hid_t compoundId = H5Tcreate(H5T_COMPOUND, sizeof(singlecomplex));
+        H5Tinsert(compoundId, "real", HOFFSET(complex_type, r), H5T_NATIVE_FLOAT);
+        H5Tinsert(compoundId, "imag", HOFFSET(complex_type, i), H5T_NATIVE_FLOAT);
+
+        hid_t dataset_id = H5Dcreate(
+            fid, h5path.c_str(), compoundId, dspace_id, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+        status = H5Dwrite(dataset_id, compoundId, H5S_ALL, H5S_ALL, H5P_DEFAULT, buffer);
+
+        H5Dclose(dataset_id);
+        H5Sclose(dspace_id);
+        if (status < 0) {
+            bSuccess = false;
+        } else {
+            bSuccess = h5SaveComplexAttribute(fid, h5path);
+        }
+    } else {
+        hid_t dataset_id = H5Dcreate(
+            fid, h5path.c_str(), type_id, dspace_id, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+        status = H5Dwrite(dataset_id, type_id, H5S_ALL, H5S_ALL, H5P_DEFAULT, buffer);
+        H5Dclose(dataset_id);
+        H5Sclose(dspace_id);
+        if (status < 0) {
+            bSuccess = false;
+        } else {
+            bSuccess = true;
+        }
+    }
+
+    if (bSuccess) {
+        bSuccess = h5SaveClassAttribute(fid, h5path, VariableValue);
+        if (!bSuccess) {
+            return false;
+        }
+        bSuccess = h5SaveDimensionsAttribute(fid, h5path, dimsValue);
+    }
     return bSuccess;
 }
 //=============================================================================
