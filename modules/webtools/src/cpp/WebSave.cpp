97953c509c7e1093c28af3e970251822b4cd253a
Allan CORNET <nelson.numerical.computation@gmail.com>
2019-08-19 20:57:39 +0200

    webread function

--- modules/webtools/src/cpp/WebSave.cpp
+++ modules/webtools/src/cpp/WebSave.cpp
@@ -557,6 +557,103 @@ encodeUrl(CURL* curlObject, const std::wstring& url, const ArrayOfVector& names,
     return urlEncoded;
 }
 //=============================================================================
+static std::wstring
+errorCodeToMessage(long response_code)
+{
+    std::wstring message;
+    switch (response_code) {
+    case 400:
+        return _W("Bad Request (400)");
+    case 401:
+        return _W("Unauthorized (401)");
+    case 402:
+        return _W("Payment Required (402)");
+    case 403:
+        return _W("Forbidden (403)");
+    case 404:
+        return _W("Not Found (404)");
+    case 405:
+        return _W("Method Not Allowed (405)");
+    case 406:
+        return _W("Not Acceptable (406)");
+    case 407:
+        return _W("Proxy Authentication Required (407)");
+    case 408:
+        return _W("Request Timeout (408)");
+    case 409:
+        return _W("Conflict (409)");
+    case 410:
+        return _W("Gone (410)");
+    case 411:
+        return _W("Length Required (411)");
+    case 412:
+        return _W("Precondition Failed (412)");
+    case 413:
+        return _W("Payload Too Large (413)");
+    case 414:
+        return _W("URI Too Long (414)");
+    case 415:
+        return _W("Unsupported Media Type (415)");
+    case 416:
+        return _W("Range Not Satisfiable (416)");
+    case 417:
+        return _W("Expectation Failed (417)");
+    case 418:
+        return _W("I'm a teapot (418)");
+    case 421:
+        return _W("Misdirected Request (421)");
+    case 422:
+        return _W("Unprocessable Entity (422)");
+    case 423:
+        return _W("Locked (423)");
+    case 424:
+        return _W("Failed Dependency (424)");
+    case 425:
+        return _W("Too Early (425)");
+    case 426:
+        return _W("Upgrade Required (426)");
+    case 428:
+        return _W("Precondition Required (428)");
+    case 429:
+        return _W("Too Many Requests (429)");
+    case 431:
+        return _W("Request Header Fields Too Large (431)");
+    case 451:
+        return _W("Unavailable For Legal Reasons (451)");
+    case 500:
+        return _W("Internal Server Error (500)");
+    case 501:
+        return _W("Not Implemented (501)");
+    case 502:
+        return _W("Bad Gateway (502)");
+    case 503:
+        return _W("Service Unavailable (503)");
+    case 504:
+        return _W("Gateway Timeout (504)");
+    case 505:
+        return _W("HTTP Version Not Supported (505)");
+    case 506:
+        return _W("Variant Also Negotiates (506)");
+    case 507:
+        return _W("Insufficient Storage (507)");
+    case 508:
+        return _W("Loop Detected (508)");
+    case 510:
+        return _W("Not Extended(510)");
+    case 511:
+        return _W("Network Authentication Required (511)");
+    default:{
+        if (response_code >= 400 && response_code < 500) {
+            _W("HTTP Client error code: ") + std::to_wstring(response_code);
+        } 
+        if (response_code >= 500 && response_code < 600) {
+            _W("HTTP Server error code: ") + std::to_wstring(response_code);
+        } 
+    } break;
+    }
+    return message;
+}
+//=============================================================================
 ArrayOf
 WebSave(const std::wstring& url, const std::wstring& filename, const ArrayOfVector& names,
     const ArrayOfVector& values, WebOptions& options, bool haveEventsLoop)
@@ -694,16 +791,6 @@ WebSave(const std::wstring& url, const std::wstring& filename, const ArrayOfVect
         Error(msg);
     }
 
-    /*
-    curlCode = curl_easy_setopt(curlObject, CURLOPT_IGNORE_CONTENT_LENGTH, 1L);
-    if (curlCode != CURLE_OK) {
-        fclose(fw);
-        std::string msg = curl_easy_strerror(curlCode);
-        curl_easy_cleanup(curlObject);
-        Error(msg);
-    }
-    */
-
     curlCode = curl_easy_setopt(curlObject, CURLOPT_FOLLOWLOCATION, 1L);
     if (curlCode != CURLE_OK) {
         fclose(fw);
@@ -729,13 +816,20 @@ WebSave(const std::wstring& url, const std::wstring& filename, const ArrayOfVect
     }
 
     curl_easy_cleanup(curlObject);
-    if (response_code == 400) {
-        Error(_W("BAD REQUEST (400)"));
-    }
-    if (response_code == 405) {
-        Error(_W("METHOD NOT ALLOWED (405)"));
-    }
+    std::wstring msg = errorCodeToMessage(response_code);
+    if (!msg.empty()) {
+        // remove file if error detected.
+        try {
+            boost::filesystem::path p = filename;
+            boost::filesystem::permissions(p,
+                boost::filesystem::add_perms | boost::filesystem::owner_write
+                    | boost::filesystem::group_write | boost::filesystem::others_write);
 
+            boost::filesystem::remove(p);
+        } catch (const boost::filesystem::filesystem_error&) {
+        }
+        Error(msg);
+    }
     return ArrayOf::characterArrayConstructor(fullFilename);
 }
 //=============================================================================
