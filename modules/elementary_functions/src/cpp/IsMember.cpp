a1b87ca735066b866a256a9e4badc221270a8b40
Allan CORNET <nelson.numerical.computation@gmail.com>
2021-05-17 10:54:10 +0200

    fix build missing include

--- modules/elementary_functions/src/cpp/IsMember.cpp
+++ modules/elementary_functions/src/cpp/IsMember.cpp
@@ -23,6 +23,7 @@
 // License along with this program. If not, see <http://www.gnu.org/licenses/>.
 // LICENCE_BLOCK_END
 //=============================================================================
+#include <cstring>
 #include "nlsConfig.h"
 #include "IsMember.hpp"
 #include "Error.hpp"
