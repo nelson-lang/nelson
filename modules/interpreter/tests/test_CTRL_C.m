61997d7f00f12cf25ed1190c7d3c3c219a603eed
Allan CORNET <nelson.numerical.computation@gmail.com>
2021-07-26 21:43:37 +0200

    CTRL-C throws an error (#478)

---
+++ modules/interpreter/tests/test_CTRL_C.m
@@ -0,0 +1,32 @@
+%=============================================================================
+% Copyright (c) 2016-present Allan CORNET (Nelson)
+%=============================================================================
+% This file is part of the Nelson.
+%=============================================================================
+% LICENCE_BLOCK_BEGIN
+% This program is free software; you can redistribute it and/or
+% modify it under the terms of the GNU Lesser General Public
+% License as published by the Free Software Foundation; either
+% version 2.1 of the License, or (at your option) any later version.
+%
+% Alternatively, you can redistribute it and/or
+% modify it under the terms of the GNU General Public License as
+% published by the Free Software Foundation; either version 2 of
+% the License, or (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Lesser General Public License for more details.
+%
+% You should have received a copy of the GNU Lesser General Public
+% License along with this program. If not, see <http://www.gnu.org/licenses/>.
+% LICENCE_BLOCK_END
+%=============================================================================
+% <--INTERACTIVE TEST-->
+% executes these commands:
+for i=1:1000;magic(6);end
+% do CTRL-C
+who
+% checks only variables available are: 'ans' and 'i'
+%=============================================================================
