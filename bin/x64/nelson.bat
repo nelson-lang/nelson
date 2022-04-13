a4da248a153f67cfafb4f49ae060e4324a8c8a1e
Allan CORNET <nelson.numerical.computation@gmail.com>
2022-04-13 21:53:10 +0200

    fix file

--- bin/x64/nelson.bat
+++ bin/x64/nelson.bat
@@ -5,8 +5,8 @@ rem ============================================================================
 rem This file is part of the Nelson.
 rem =============================================================================
 rem # LICENCE_BLOCK_BEGIN
-# SPDX-License-Identifier: LGPL-3.0-or-later
-# LICENCE_BLOCK_END
+rem # SPDX-License-Identifier: LGPL-3.0-or-later
+rem # LICENCE_BLOCK_END
 rem =============================================================================
 SETLOCAL ENABLEEXTENSIONS
 SET PARENT=%~dp0
