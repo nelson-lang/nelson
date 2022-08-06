d0be484725648e5ad687c16c9c9495d90178b6a5
Allan CORNET <nelson.numerical.computation@gmail.com>
2022-08-06 15:47:22 +0200

    sinc function

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,6 +15,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
   - `hamming`: Hamming window.
   - `bartlett`: Bartlett window.
   - `blackman`: Blackman window.
+  - `sinc`: sinc function.
 
 ### Changed
 
--- modules/signal_processing/functions/sinc.m
+++ modules/signal_processing/functions/sinc.m
@@ -7,23 +7,12 @@
 % SPDX-License-Identifier: LGPL-3.0-or-later
 % LICENCE_BLOCK_END
 %=============================================================================
-function c = bartlett(varargin)
-  % Oppenheim, Alan V., Ronald W. Schafer, and John R. Buck. Discrete-Time Signal Processing. Upper Saddle River, NJ: Prentice Hall, 1999, pp.
+function c = sinc(varargin)
   narginchk(1, 1);
   m = varargin{1};
-  isPositiveIntegerOrEmpty = isempty(m) || (isscalar(m) && (m == fix (m)) && (m > 0));
-  if ~isPositiveIntegerOrEmpty
-    error(_('M must be a positive integer.'));
-  end
-  if isempty(m)
-    c = zeros(0, 1);
-  elseif (m ~= 1)
-    m = m - 1;
-    n = fix (m / 2);
-    c = [2*(0:n)/m, 2-2*(n+1:m)/m]';
-  else
-    c = 1;
-  end
+  i = find(m == 0);                                                              
+  m(i) = 1;
+  c = sin(pi * m) ./ (pi * m);                                                     
+  c(i) = 1;
 end
 %=============================================================================
-  
