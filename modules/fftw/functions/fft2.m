f98c7c6250b52fce2687ad237c190e63143b4b2f
Allan CORNET <nelson.numerical.computation@gmail.com>
2022-08-07 20:54:22 +0200

    fft2 function

---
+++ modules/fftw/functions/fft2.m
@@ -0,0 +1,32 @@
+%=============================================================================
+% Copyright (c) 2016-present Allan CORNET (Nelson)
+%=============================================================================
+% This file is part of the Nelson.
+%=============================================================================
+% LICENCE_BLOCK_BEGIN
+% SPDX-License-Identifier: LGPL-3.0-or-later
+% LICENCE_BLOCK_END
+%=============================================================================
+function y = fft2(x, r, c)
+  % 2-D fast Fourier transform
+  % y = fft2(X)
+  % Y = fft2(X, m, n)
+  if ~(nargin() == 1 || nargin() == 3)
+    error(_('Wrong number of input arguments.'));
+  end
+  nargoutchk(0, 1);
+  if ~ismatrix(x)
+    if nargin == 1
+      y = fft(fft(x, [], 2), [], 1);
+    else
+      y = fft(fft(x, c, 2), r, 1);
+    end
+  else
+    if nargin==1
+      y = fftn(x);
+    else
+      y = fftn(x, [r, c]);
+    end
+  end   
+  %=============================================================================
+  
