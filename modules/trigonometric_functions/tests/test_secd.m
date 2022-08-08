583162514be55432fdf154e4ae354d1ce6a37ead
Allan CORNET <nelson.numerical.computation@gmail.com>
2022-08-08 18:22:52 +0200

    secd function

--- modules/trigonometric_functions/tests/test_secd.m
+++ modules/trigonometric_functions/tests/test_secd.m
@@ -7,10 +7,10 @@
 % SPDX-License-Identifier: LGPL-3.0-or-later
 % LICENCE_BLOCK_END
 %=============================================================================
-function y = sech (varargin)
-  narginchk(1, 1);
-  nargoutchk(0, 1);
-  x = cosh(varargin{1});
-  y = ones('like', x) ./ x;
-end
+assert_isequal(nargin('secd'), -1);
+assert_isequal(nargout('secd'), 1);
+%=============================================================================
+R = secd([1, 10+3i, 15+2i, 35+i]);
+REF = [1.0002 + 0.0000i,   1.0140 + 0.0094i,   1.0346 + 0.0097i,   1.2204 + 0.0149i];
+assert_isapprox(R, REF, 1e-4);
 %=============================================================================
