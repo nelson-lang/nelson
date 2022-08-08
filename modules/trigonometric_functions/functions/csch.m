c6fb8328451966f341949e2c78d1f96ceff1d462
Allan CORNET <nelson.numerical.computation@gmail.com>
2022-08-08 19:48:15 +0200

    csch function

--- modules/trigonometric_functions/functions/csch.m
+++ modules/trigonometric_functions/functions/csch.m
@@ -7,11 +7,11 @@
 % SPDX-License-Identifier: LGPL-3.0-or-later
 % LICENCE_BLOCK_END
 %=============================================================================
-assert_isequal(nargin('secd'), -1);
-assert_isequal(nargout('secd'), 1);
+function x = csch(varargin)
+    narginchk(1, 1);
+    nargoutchk(0, 1);
+    x = sinh(varargin{1});
+    x = ones('like', x) ./ x;
+end
 %=============================================================================
-R = secd([1, 10+3i, 15+2i, 35+i]);
-REF = [1.0002 + 0.0000i,   1.0140 + 0.0094i,   1.0346 + 0.0097i,   1.2204 + 0.0149i];
-assert_isapprox(real(R), real(REF), 1e-4);
-assert_isapprox(imag(R), imag(REF), 1e-2);
-%=============================================================================
+    
