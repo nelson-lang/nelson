fab5ea9bd8ff189a524e5156337e247a37f253b4
Allan CORNET <nelson.numerical.computation@gmail.com>
2022-08-08 21:01:01 +0200

    rad2deg function

---
+++ modules/trigonometric_functions/functions/rad2deg.m
@@ -0,0 +1,21 @@
+%=============================================================================
+% Copyright (c) 2016-present Allan CORNET (Nelson)
+%=============================================================================
+% This file is part of the Nelson.
+%=============================================================================
+% LICENCE_BLOCK_BEGIN
+% SPDX-License-Identifier: LGPL-3.0-or-later
+% LICENCE_BLOCK_END
+%=============================================================================
+function y = rad2deg(varargin)
+    narginchk(1, 1);
+    nargoutchk(0, 1);
+    x = varargin{1};
+    if isfloat(x)
+        y = (180 / pi) * x;
+    else
+        error('Nelson:rad2deg:nonFloatInput', _('Input must be single or double.'));
+    end
+end
+%=============================================================================
+    
