fe87c29cdc685e23d70d4e5e9b64a79195863573
Allan CORNET <nelson.numerical.computation@gmail.com>
2022-08-09 20:15:37 +0200

    asinh function

---
+++ modules/trigonometric_functions/functions/asinh.m
@@ -0,0 +1,22 @@
+%=============================================================================
+% Copyright (c) 2016-present Allan CORNET (Nelson)
+%=============================================================================
+% This file is part of the Nelson.
+%=============================================================================
+% LICENCE_BLOCK_BEGIN
+% SPDX-License-Identifier: LGPL-3.0-or-later
+% LICENCE_BLOCK_END
+%=============================================================================
+function y = asinh(varargin)
+    narginchk(1, 1);
+    nargoutchk(0, 1);
+
+    x = varargin{1};
+    if isreal(x)
+        y = imag(asin(x * i));
+    else
+        y = -i * (asin(x * i));
+    end
+end
+%=============================================================================
+  
