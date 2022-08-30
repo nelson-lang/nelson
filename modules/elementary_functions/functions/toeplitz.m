305d6bc9c29e9b362d82976e8b238b426947c080
Allan CORNET <nelson.numerical.computation@gmail.com>
2022-08-30 19:36:19 +0200

    toeplitz, orth, rref, subspace functions

---
+++ modules/elementary_functions/functions/toeplitz.m
@@ -0,0 +1,32 @@
+function t = toeplitz(varargin)
+  narginchk(1, 2)
+  nargoutchk(0, 1)
+  
+  c = varargin{1};
+  if (nargin == 1)
+    if ~isvector(c)
+      error(_('Input argument must be a vector.'));
+    end
+    c(1) = conj(c(1));
+    r = c;
+    c = conj(c);
+  else
+    r = varargin{2};
+  end
+  if ~(isnumeric(c) || isnumeric(r))
+    error(_('Input arguments must be numeric.'));
+  end
+  if ~isempty(c) && ~isempty(r) && ~isequaln(r(1),c(1))
+    warning(_('Column wins diagonal conflict.'))
+  end
+  c = c(:);
+  m = length(c);    
+  r = r(:);
+  p = length(r);
+  x = [r(p:-1:2, 1) ; c];
+  idx = (0 : m-1)' + (p:-1:1);
+  t = x(idx);
+  if ~isempty(t) && isrow(idx)
+    t = t.';
+  end
+end
---
+++ modules/linear_algebra/functions/rref.m
@@ -0,0 +1,55 @@
+%=============================================================================
+% Copyright (c) 2016-present Allan CORNET (Nelson)
+%=============================================================================
+% This file is part of the Nelson.
+%=============================================================================
+% LICENCE_BLOCK_BEGIN
+% SPDX-License-Identifier: LGPL-3.0-or-later
+% LICENCE_BLOCK_END
+%=============================================================================
+function varargout = rref (varargin)
+  narginchk(1, 2);
+  nargoutchk(0, 2);
+  A = varargin{1};
+  if (ndims (A) > 2)
+    error (_('Inputs must be 2-D.'));
+  end
+  [rows, cols] = size (A);
+  
+  if (nargin == 2)
+    tol = varargin{2};
+  else
+    tol = max(rows, cols) * eps(class(A)) * norm(A, inf);
+  end
+
+  if isempty(A)
+    varargout{1} = A;
+    if nargout > 1
+      varargout{2} = zeros(1, 0);
+    end
+    return
+  end
+  used = zeros (1, cols);
+  r = 1;
+  for c = 1:cols
+    [m, p] = max(abs(A(r:rows,c)));
+    if (m > tol)
+      used(1, c) = 1;
+      ccols = c:cols;
+      A(r, ccols) = A(r, ccols) / A(r, c);
+      ridx = [1:r - 1, r + 1:rows];
+      A(ridx, ccols) = A(ridx, ccols) - A(ridx, c) * A(r, ccols);
+      if (r == rows)
+        break;
+      end
+      r = r + 1;
+    else
+      A(r:rows, c) = zeros(rows - r + 1, 1);
+    end
+  end
+  varargout{1} = A;
+  if nargout > 1
+    varargout{2} = find(used);
+  end
+end
+%=============================================================================
