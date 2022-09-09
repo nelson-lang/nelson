c9249699a8561c67ddcb281abd55c1a386af709b
Allan CORNET <nelson.numerical.computation@gmail.com>
2022-09-09 20:38:56 +0200

    adapt test mac arm m1

--- modules/trigonometric_functions/tests/test_asech.m
+++ modules/trigonometric_functions/tests/test_asech.m
@@ -12,5 +12,6 @@ assert_isequal(nargout('asech'), 1);
 %=============================================================================
 R = asech([2, -3, 1+2i]);
 REF = [0.0000 + 1.0472i,   0.0000 + 1.9106i,   0.3966 - 1.3845i];
-assert_isapprox(R, REF, 1e-4);
+assert_isapprox(abs(real(R)), abs(real(REF)), 1e-4);
+assert_isapprox(abs(imag(R)), abs(imag(REF)), 1e-4);
 %=============================================================================
