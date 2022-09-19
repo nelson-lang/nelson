dc2d34464a20d64367ebdae1b0cfe5a50580492b
Allan CORNET <nelson.numerical.computation@gmail.com>
2022-09-19 21:42:39 +0200

    fix #741 'ones' and 'zeros' do not manager logical as typename. (#742)

---
+++ modules/constructors_functions/tests/bug_github_issue_#741.m
@@ -0,0 +1,26 @@
+%=============================================================================
+% Copyright (c) 2017-2018 Allan CORNET (Nelson)
+%=============================================================================
+% This file is part of the Nelson.
+%=============================================================================
+% LICENCE_BLOCK_BEGIN
+% SPDX-License-Identifier: LGPL-3.0-or-later
+% LICENCE_BLOCK_END
+%=============================================================================
+% <-- Issue URL -->
+% https://github.com/Nelson-numerical-software/nelson/issues/741
+% <-- Short Description -->
+% 'ones' and 'zeros' do not manager logical as typename.
+%=============================================================================
+R = ones([3 4], 'logical');
+REF = cast(ones(3, 4), 'logical');
+assert_isequal(R, REF);
+%=============================================================================
+R = zeros([3 4], 'logical');
+REF = cast(zeros(3, 4), 'logical');
+assert_isequal(R, REF);
+%=============================================================================
+R = eye([3 4], 'logical');
+REF = cast(eye(3, 4), 'logical');
+assert_isequal(R, REF);
+%=============================================================================
