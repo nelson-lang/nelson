bbc6947063c80d3e094ee4a738f49d72916e8ce9
Allan CORNET <nelson.numerical.computation@gmail.com>
2022-11-06 11:20:14 +0100

    noaudio

--- modules/tests_manager/functions/test_run.m
+++ modules/tests_manager/functions/test_run.m
@@ -194,7 +194,7 @@ function test_suite = process_files_to_test(test_files_list, option, classname, 
   parallel_tests = [];
   sequential_tests = [];
   for file = test_files_list(:)'
-    test_case = create_test_case(file{1});
+    test_case = create_test_case(file{1}, classname);
     test_case.classname = classname;
     if test_case.options.sequential_test_required || test_case.isbench
       sequential_tests = [sequential_tests, test_case];
@@ -340,19 +340,19 @@ function r = isSupportedPlatform(test_case)
   end
 end
 %=============================================================================
-function test_case = create_test_case(filename)
+function test_case = create_test_case(filename, classname)
   test_case.filename = filename;
   test_case.options = test_parsetags(filename);
   test_case.msg = '';
   
   [p, f, e] = fileparts(filename);
   test_case.name = f;
-  test_case.classname = '';
+  test_case.classname = classname;
   test_case.status = ''; % 'Pass', 'Fail', or 'Skip'. '' not evaluated.
   test_case.time = 0;
   test_case.isbench = isbench(filename);
   test_case.skip = false;
-
+  
   if test_case.isbench
     timeout = int2str(380);
   else
@@ -439,12 +439,12 @@ function test_case = create_test_case(filename)
         cmd = [cmd, ' ', '--noipc'];
       end
       redirect_to_file = [' 2>&1 "' , redirect_err, '"'];
-
-      without_audio = ~(test_case.options.audio_input_required || test_case.options.audio_output_required);
+      
+      without_audio = ~(test_case.options.audio_input_required || test_case.options.audio_output_required) && ~endsWith(test_case.classname, '.audio');
       if without_audio
         cmd = [cmd, ' ', '--noaudio'];
       end
-
+      
       cmd = [cmd, ' --quiet', ' ', '--nouserstartup', ' ', '--timeout', ' ', timeout, ' ', '--file', ' "', command_filename, '" ', redirect_to_file];
       if test_case.options.gui_mode
         test_case.command = build_command_nelson_gui(cmd, test_case.options.mpi_mode);
