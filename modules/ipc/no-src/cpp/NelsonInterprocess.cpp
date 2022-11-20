37078c22aae97f3e9a71b9276903589ef3106cf1
Allan CORNET <nelson.numerical.computation@gmail.com>
2022-11-20 20:00:42 +0100

    fix no-ipc (#786)

---
+++ modules/ipc/no-src/cpp/NelsonInterprocess.cpp
@@ -0,0 +1,64 @@
+//=============================================================================
+// Copyright (c) 2016-present Allan CORNET (Nelson)
+//=============================================================================
+// This file is part of the Nelson.
+//=============================================================================
+// LICENCE_BLOCK_BEGIN
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// LICENCE_BLOCK_END
+//=============================================================================
+#include "NelsonInterprocess.hpp"
+//=============================================================================
+namespace Nelson {
+//=============================================================================
+bool
+sendVariableToNelsonInterprocessReceiver(int pidDestination, const ArrayOf& var,
+    const std::wstring& name, const std::wstring& scope, bool withEventsLoop,
+    std::wstring& errorMessage)
+{
+    return false;
+}
+//=============================================================================
+bool
+createNelsonInterprocessReceiver(int pid, bool withEventsLoop)
+{
+    return false;
+}
+//=============================================================================
+bool
+isMinimizedFromNelsonInterprocessReceiver(
+    int pidDestination, bool withEventsLoop, std::wstring& errorMessage)
+{
+    return false;
+}
+//=============================================================================
+bool
+removeNelsonInterprocessReceiver(int pid, bool withEventsLoop)
+{
+    return false;
+}
+//=============================================================================
+bool
+evalCommandToNelsonInterprocessReceiver(int pidDestination, const std::wstring& command,
+    bool withEventsLoop, std::wstring& result, std::wstring& errorMessage)
+{
+
+    return false;
+}
+//=============================================================================
+ArrayOf
+getVariableFromNelsonInterprocessReceiver(int pidDestination, const std::wstring& name,
+    const std::wstring& scope, bool withEventsLoop, std::wstring& errorMessage)
+{
+    return {};
+}
+//=============================================================================
+bool
+sendMinimizeToNelsonInterprocessReceiver(
+    int pidDestination, bool minimize, bool withEventsLoop, std::wstring& errorMessage)
+{
+    return false;
+}
+//=============================================================================
+}
+//=============================================================================
