9989e389a2005f8c86240f66b9072856a6d6dce0
Allan CORNET <nelson.numerical.computation@gmail.com>
2023-01-17 06:46:56 +0100

    fix #834 VariableCompleter was not filtered by prefix. (#835)

---
+++ modules/text_editor/tests/bug_github_issue_#834.m
@@ -0,0 +1,23 @@
+%=============================================================================
+% Copyright (c) 2017 Allan CORNET (Nelson)
+%=============================================================================
+% This file is part of the Nelson.
+%=============================================================================
+% LICENCE_BLOCK_BEGIN
+% SPDX-License-Identifier: LGPL-3.0-or-later
+% LICENCE_BLOCK_END
+%=============================================================================
+% <-- Issue URL -->
+% https://github.com/Nelson-numerical-software/nelson/issues/834
+% <-- Short Description -->
+% VariableCompleter was not filtered by prefix.
+%=============================================================================
+% <--INTERACTIVE TEST-->
+%=============================================================================
+A = 3;
+AA = 33;
+AAV = 31;
+B = 2;
+BB = 32;
+edit
+% type A<TAB>, you should see only A && AAV as proposed text
