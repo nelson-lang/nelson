6d5d80d47cfdff53f3ab631bc77997926b478fa9
Allan CORNET <nelson.numerical.computation@gmail.com>
2023-10-15 21:07:12 +0200

    mrdivide tf, ss

--- modules/control_system/functions/@ss/mrdivide.m
+++ modules/control_system/functions/@ss/mrdivide.m
@@ -7,23 +7,9 @@
 % SPDX-License-Identifier: LGPL-3.0-or-later
 % LICENCE_BLOCK_END
 %=============================================================================
-function res = inv(sys)
-  A = sys.A;
-  B = sys.B;
-  C = sys.C;
-  D = sys.D;
-  E = sys.E;
-  
-  res = sys;
-  m = size(B, 2);
-  n = size(A, 1);
-  res.A = [A, B; C, D];
-  res.B = [zeros(n, m); -eye(m)];
-  res.C = [zeros(m, n), eye(m)];
-  res.D = zeros(m, m);
-  if isempty(E)
-    E = eye(n, n);
-  end
-  res.E = [E, zeros(n, m); zeros(m, n + m)];
+function varargout = mrdivide(varargin)
+  sys1 = ss(varargin{1});
+  sys2 = ss(varargin{2});
+  varargout{1} = sys1 * inv(sys2);
 end
 %=============================================================================
