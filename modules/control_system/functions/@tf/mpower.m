16dba18f6cb1be68372dcc659f09ffd228c89737
Allan CORNET <nelson.numerical.computation@gmail.com>
2023-10-17 20:27:33 +0200

    tf, ss mpower

--- modules/control_system/functions/@tf/mpower.m
+++ modules/control_system/functions/@tf/mpower.m
@@ -7,12 +7,28 @@
 % SPDX-License-Identifier: LGPL-3.0-or-later
 % LICENCE_BLOCK_END
 %=============================================================================
-function varargout = mldivide(varargin)
-  narginchk(2, 2),
-  nargoutchk(0, 1);
+function varargout = mpower(varargin)
+  narginchk(2, 2)
+  nargoutchk(0, 1)
   sysA = tf(varargin{1});
-  sysB = tf(varargin{2});
-  sys = inv(sysA) * sysB;
+  n = varargin{2};
+  mustBeNumeric(n, 2);
+  mustBeInteger(n, 2);
+  mustBeScalarOrEmpty(n, 2);
+  mustBeNonempty(n, 2);
+  if (n == 0)
+    varargout{1} = ss(1);
+    return;
+  end
+  
+  if (n < 0)
+    n = -n;
+    sysA = inv(sysA);
+  end
+  sys = sysA;
+  for i = 2:n
+    sys = sys * sysA;
+  end
   varargout{1} = sys;
 end
 %=============================================================================
