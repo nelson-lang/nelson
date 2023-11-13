410a6bae2642b94d81dc3a409c2d4241d6912f93
Allan CORNET <nelson.numerical.computation@gmail.com>
2023-11-13 21:13:43 +0100

    subsref

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 - `stem` Plot discrete sequence data.
 - `stairs` Stairstep graph.
 - `fill` 2-D patch.
+- `subsref` Subscripted reference.
 
 ### Changed
 
---
+++ modules/operators/functions/subsref.m
@@ -0,0 +1,29 @@
+%=============================================================================
+% Copyright (c) 2023-present Allan CORNET (Nelson)
+%=============================================================================
+% This file is part of the Nelson.
+%=============================================================================
+% LICENCE_BLOCK_BEGIN
+% SPDX-License-Identifier: LGPL-3.0-or-later
+% LICENCE_BLOCK_END
+%=============================================================================
+function varargout = subsref(varargin)
+  narginchk(2, 2);
+  A = varargin{1};
+  S = varargin{2};
+  mustBeA(S, 'struct', 2);
+  for k = 1:numel(S)
+    switch (S(k).type)
+      case '()'
+        R = {A(S(k).subs{:})};
+      case '{}'
+        R = {A{S(k).subs{:}}};
+      case '.'
+        R = {A.(S(k).subs)};
+      otherwise
+        error(_('Illegal indexing structure argument: type ''.'', ''{}'' or ''()'' expected.'));
+    end
+   end
+   varargout = R;
+end
+%=============================================================================
