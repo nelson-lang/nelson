de7ea3d4192c4a84f5319c514acda19d9aee437e
Allan CORNET <nelson.numerical.computation@gmail.com>
2023-11-26 20:58:33 +0100

    fix tf mrdivide and tf('s')

---
+++ modules/control_system/functions/@tf/length.m
@@ -0,0 +1,17 @@
+%=============================================================================
+% Copyright (c) 2023-present Allan CORNET (Nelson)
+%=============================================================================
+% This file is part of the Nelson.
+%=============================================================================
+% LICENCE_BLOCK_BEGIN
+% SPDX-License-Identifier: LGPL-3.0-or-later
+% LICENCE_BLOCK_END
+%=============================================================================
+function varargout = length(varargin)
+  narginchk(1, 1);
+  nargoutchk(0, 1);
+  [m, n] =  size(varargin{1}.Numerator);
+  varargout{1} = m*n;
+end
+%=============================================================================
+
