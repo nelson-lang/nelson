0fe6bb976ed44025dda83b730d81ff50460e3c06
Allan CORNET <nelson.numerical.computation@gmail.com>
2023-12-02 11:25:59 +0100

    fix #3 Invalid extraction & insertion on function_handle

---
+++ modules/interpreter/tests/bug_github_issue_#3.m
@@ -0,0 +1,28 @@
+%=============================================================================
+% Copyright (c) 2017 Allan CORNET (Nelson)
+%=============================================================================
+% This file is part of the Nelson.
+%=============================================================================
+% LICENCE_BLOCK_BEGIN
+% SPDX-License-Identifier: LGPL-3.0-or-later
+% LICENCE_BLOCK_END
+%=============================================================================
+% <-- Issue URL -->
+% https://github.com/nelson-lang/nelson/issues/3
+% <-- Short Description -->
+% Invalid extraction & insertion on function_handle.
+%=============================================================================
+A = str2func('sin');
+%=============================================================================
+assert_checkerror('A(1).b= 1', _('Cannot apply A.field_name = B to non struct-array object A.'));
+%=============================================================================
+assert_checkerror('A(1).b', _('Invalid indexing.'));
+%=============================================================================
+assert_checkerror('A.b = 3', _('Cannot apply A.field_name = B to non struct-array object A.'));
+%=============================================================================
+assert_checkerror('A{1}', _('Attempt to apply contents-indexing to non cell-array object.'));
+%=============================================================================
+assert_checkerror('A{1}', _('Attempt to apply contents-indexing to non cell-array object.'));
+%=============================================================================
+assert_checkerror('A{1} = 1', _('Cannot convert base types to reference types.'));
+%=============================================================================
