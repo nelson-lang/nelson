18bc7cb20f357e5128e0fdcfa38effc099cd22ed
Allan CORNET <nelson.numerical.computation@gmail.com>
2023-12-10 20:44:26 +0100

    tf2ss and ss2tf (compatibility, tests, help)

--- modules/control_system/functions/tf2ss.m
+++ modules/control_system/functions/tf2ss.m
@@ -1,128 +1,100 @@
-% SPDX-License-Identifier: MIT
-% Transform transfer function to state space model
-% Input: G, canonical form(optional)
-% Example 1: sys = tf2ss(G) % Observable canonical form is used as default
-% Example 2: sys = tf2ss(G, 'OCF') % Observable canonical form
-% Example 3: sys = tf2ss(G, 'CCF') % Controllable canonical form
-% Author: Daniel Mårtensson, 2017 September
+%=============================================================================
+% Copyright (c) 2017 September Daniel Mårtensson (Swedish Embedded Control Systems Toolbox)
+% Copyright (c) 2023-present Allan CORNET (Nelson)
+%=============================================================================
+% This file is part of Nelson.
+%=============================================================================
+% LICENCE_BLOCK_BEGIN
+% SPDX-License-Identifier: LGPL-3.0-or-later
+% LICENCE_BLOCK_END
+%=============================================================================
+function varargout = tf2ss(numerator, denominator)
+  narginchk(2, 2);
+  nargoutchk(0, 4);
+  
+  % Ensure the denominator is a row vector
+  denominator = denominator(:)';
+  if issingle(numerator) || issingle(denominator)
+    numerator = single(numerator);
+    denominator = single(denominator);
+  end
+  if isempty(numerator) && isempty(denominator)
+    A = zeros(0, 'like', numerator);
+    B = zeros(0, 'like', numerator);
+    C = zeros(0, 'like', numerator);
+    D = zeros(0, 'like', numerator);
+  else 
+    % Normalize transfer function to have a leading coefficient of 1
+    numerator = numerator / denominator(1);
+    denominator = denominator / denominator(1);
+    
+    % Ensure lengths of numerator and denominator match
+    numerator = padNumeratorWithZeros(numerator, denominator);
+    
+    % Remove leading zero columns
+    denominator = removeLeadingZeroColumns(denominator);
+    numerator = removeLeadingZeroColumns(numerator);
+    
+    % Remove leading zeros from the denominator
+    idx = find(denominator);
+    if ~isempty(idx)
+      denominator = denominator(idx(1):end);
+    end
+  
+    [mb, nb] = size(numerator);
+    na = size(denominator, 2);
+  
+    % Pad numerator with zeros if necessary
+    if nb < na
+      numerator = padNumeratorWithZeros(numerator, na);
+    end
+  
+    [A, B, C, D] = computeStateSpaceMatrices(numerator, denominator, mb, nb, na);
+  end
 
-function [sys] = tf2ss(varargin)
-	% Check if there is a MIMO TF model
-	if(or(size(varargin{1}, 1) > 1, size(varargin{1}, 2) > 1))
-		error('Only for SISO tranfer functions!')
-	end
-
-	% Check if there is some input arguments or it's not a transfer function
-	if(or(isempty(varargin), ~strcmp(varargin{1}.type, 'TF' )))
-		error ('Missing transfer function')
-	end
-
-	% Get necessary info
-	numerator = varargin{1}.num;
-	denomerator = varargin{1}.den;
-	delay = varargin{1}.delay;
-
-	% Check if the numerator is longer that denomerator
-	if(length(numerator) > length(denomerator))
-		error('Numerator cannot be longer that denomerator - No existing state space model');
-	end
-
-	% Make sure that the highest order element in the denomerator has the scalar 1 - Proper transfer function
-	% Divide all the elements in numerator and denomerator with denomerator(1)
-	numerator = numerator./(denomerator(1));
-	denomerator = denomerator./(denomerator(1));
-
-	% Make sure that the length of the numerators is equal to the length of denomerator
-	if(length(numerator) < length(denomerator))
-		% Add 0's at the numerator
-		numerator = [zeros(1, length(denomerator) - length(numerator)) numerator];
-		% Check if form is CCF or OCF
-		if(length(varargin) >= 2)
-			if(strcmp(varargin{2},'CCF'))
-				[A, B, C, D] = controllable_canonical_form(numerator,denomerator);
-			elseif(strcmp(varargin{2},'OCF'))
-				[A, B, C, D] = observable_canonical_form(numerator,denomerator);
-			else
-				txt = sprintf('Warning: Does not reginoze the canonical form %s. Observable canonical form is used\n', varargin{2});
-				disp(txt);
-				[A, B, C, D] = observable_canonical_form(numerator,denomerator);
-			end
-		else
-			txt = sprintf('Converting from transfer function to state space\nObservable canonical form is used as default\n');
-			disp(txt);
-			[A, B, C, D] = observable_canonical_form(numerator,denomerator);
-		end
-	else
-		% They are equal - No add 0's from the start
-		% Check if form is CCF or OCF
-		if(length(varargin) >= 2)
-			if(strcmp(varargin{2},'CCF'))
-				[A, B, C, D] = controllable_canonical_form(numerator,denomerator);
-			elseif(strcmp(varargin{2},'OCF'))
-				[A, B, C, D] = observable_canonical_form(numerator,denomerator);
-			else
-				txt = sprintf('Warning: Does not reginoze the canonical form %s. Observable canonical form is used\n', varargin{2});
-				disp(txt);
-				[A, B, C, D] = observable_canonical_form(numerator,denomerator);
-			end
-		else
-			txt = sprintf('Converting from transfer function to state space\nObservable canonical form is used as default\n');
-			disp(txt);
-			[A, B, C, D] = observable_canonical_form(numerator,denomerator);
-		end
-	end
-
-	% Check if A, B, C, D matrecis contains nothing. Give them zero then!
-	if(or(size(A,1) == 0, size(A,2) == 0))
-		A = [0];
-	end
-
-	if(or(size(B,1) == 0, size(B,2) == 0))
-		B = [0];
-	end
-
-	if(or(size(C,1) == 0, size(C,2) == 0))
-		C = [0];
-	end
-
-	if(or(size(D,1) == 0, size(D,2) == 0))
-		D = [0];
-	end
-
-
-	% Now everything is done!
-	sys = ss(delay, A, B,C, D);
-	sys.sampleTime = varargin{1}.sampleTime;
+  % Output state-space matrices
+  varargout{1} = A;
+  if nargout > 1
+    varargout{2} = B;
+  end
+  if nargout > 2
+    varargout{3} = C;
+  end
+  if nargout > 3
+    varargout{4} = D;
+  end
 end
-
-% This function generates matrix A, B, C, D from observable canonical form
-function [A, B, C, D] = observable_canonical_form(numerator,denomerator)
-	% Matrix D
-	D = numerator(1); % If numerator == denomerator ---> D > 0, else D = 0
-	% Matrix B - Notice that "- denomerator(1,2:(length(denomerator)))'*D" are included because the D
-	B = [numerator(1,2:(length(numerator)))' - denomerator(1,2:(length(denomerator)))'*D];
-	% Matrix C
-	C = [1 zeros(1, size(B, 1) - 1)]; % Minus -1 because we add 1
-	% Matrix A
-	A1 = -denomerator(1,2:(length(denomerator)))';
-	A2 = eye(size(B, 1) - 1);
-	A3 = zeros(size(C', 1) - 1, 1)';
-	A4 = [A2; A3];
-	A = [A1 A4];
+%=============================================================================
+function numerator = padNumeratorWithZeros(numerator, targetLength)
+  if length(numerator) < targetLength
+    % Pad numerator with zeros
+    numerator = [zeros(1, targetLength - length(numerator)), numerator];
+  end
 end
-
-% This function generates matrix A, B, C, D from controllable canonical form
-function [A, B, C, D] = controllable_canonical_form(numerator,denomerator)
-	% Matrix D
-	D = numerator(1); % If numerator == denomerator ---> D > 0, else D = 0
-	% Matrix C - Notice that "- denomerator(1,2:(length(denomerator)))'*D" are included because the D
-	C = [numerator(1,2:(length(numerator)))' - denomerator(1,2:(length(denomerator)))'*D]';
-	% Matrix B
-	B = [1 zeros(1, size(C, 2) - 1)]'; % Minus -1 because we add 1
-	% Matrix A
-	A1 = -denomerator(1,2:(length(denomerator)));
-	A2 = eye(size(B, 1) - 1);
-	A3 = zeros(size(C', 1) - 1, 1);
-	A4 = [A2 A3];
-	A = [A1; A4];
+%=============================================================================
+function [A, B, C, D] = computeStateSpaceMatrices(numerator, denominator, mb, nb, na)
+  if na == 1
+    A = [];
+    B = zeros(0, 1);
+    C = zeros(mb, 0);
+    D = numerator / denominator;
+  else
+    numerator = numerator / denominator(1);
+    denominator = denominator(2:na) / denominator(1);
+    A = [-denominator; eye(na - 2, na - 1)];
+    B = eye(na - 1, 1);
+    C = numerator(:, 2:nb) - diag(numerator(:, 1)) * repmat(denominator, mb, 1);
+    D = numerator(:, 1);
+  end
 end
+%=============================================================================
+function p = removeLeadingZeroColumns(p)
+  [n, m] = size(p);
+  % Check if there are multiple columns and the norm of the first column is zero
+  if m > 1 && norm(p(:, 1)) == 0
+    % Remove the first column and recursively call removeLeadingZeroColumns
+    p(:, 1) = [];
+    p = removeLeadingZeroColumns(p);
+  end
+end
+%=============================================================================
