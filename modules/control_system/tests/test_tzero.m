cc542a70feb8dc39ce015f89dc4feb35d52fe8e0
Allan CORNET <nelson.numerical.computation@gmail.com>
2023-12-10 18:02:41 +0100

    tzero function (rework, help, tests)

---
+++ modules/control_system/tests/test_tzero.m
@@ -0,0 +1,26 @@
+%=============================================================================
+% Copyright (c) 2016-present Allan CORNET (Nelson)
+%=============================================================================
+% This file is part of the Nelson.
+%=============================================================================
+% LICENCE_BLOCK_BEGIN
+% SPDX-License-Identifier: LGPL-3.0-or-later
+% LICENCE_BLOCK_END
+%=============================================================================
+A = [1 2; 3 4];
+B = [1; 0];
+C = [1 0];
+D = 0;
+sys = ss(A, B, C, D);
+z = tzero(sys);
+assert_isequal(z, 4);
+[z, nrank] = tzero(sys);
+assert_isequal(z, 4);
+assert_isapprox(nrank, 1, 1e-4);
+%=============================================================================
+z = tzero(A, B, C, D);
+assert_isequal(z, 4)
+[z, nrank] = tzero(A, B, C, D);
+assert_isequal(z, 4);
+assert_isapprox(nrank, 1, 1e-4);
+%=============================================================================
