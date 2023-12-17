434a5b4d31071c28b08278bde593e4f6092fcfcc
Allan CORNET <nelson.numerical.computation@gmail.com>
2023-12-17 16:28:44 +0100

    balreal (reworked, tests, help)

--- modules/control_system/functions/@tf/balreal.m
+++ modules/control_system/functions/@tf/balreal.m
@@ -1,5 +1,4 @@
 %=============================================================================
-% Copyright (c) 2017 October Daniel Mårtensson (Swedish Embedded Control Systems Toolbox)
 % Copyright (c) 2023-present Allan CORNET (Nelson)
 %=============================================================================
 % This file is part of the Nelson.
@@ -9,34 +8,18 @@
 % LICENCE_BLOCK_END
 %=============================================================================
 function varargout = balreal(varargin)
-  narginchk(3,3);
-  nargoutchk(0, 6);
-  
-  A = varargin{1};
-  B = varargin{2};
-  C = varargin{3};
-  rD = size(C, 1);
-  cD = size(D, 2);
-  D = zeros(rD, cD);
-  sysIn = ss(A, B, C, D);
-  
-  [sysOut, g, Ti, T] = balreal(sysIn);
-  [abal, bbal, cbal] = ssdata(sysOut);
-  
-  varargout{1} = abal;
+  narginchk(1, 1);
+  nargoutchk(0, 4);
+  sys = ss(varargin{1});
+  [sysb, g, T, Ti] = balreal(sys);
+  varargout{1} = sysb;
   if nargout > 1
-    varargout{2} = bbal;
+    varargout{2} = g;
   end
   if nargout > 2
-    varargout{3} = cbal;
+    varargout{3} = T;
   end
   if nargout > 3
-    varargout{4} = g;
-  end
-  if nargout > 4
-    varargout{5} = T;
-  end
-  if nargout > 5
-    varargout{6} = Ti;
+    varargout{4} = Ti;
   end
 end
