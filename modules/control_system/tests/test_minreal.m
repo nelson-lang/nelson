8bf8d33617f338a40f89266273b28ec4a88dafe9
Allan CORNET <nelson.numerical.computation@gmail.com>
2023-12-20 16:13:49 +0100

    fix tests precision

--- modules/control_system/tests/test_minreal.m
+++ modules/control_system/tests/test_minreal.m
@@ -31,11 +31,11 @@ A = [ -1   0   0   0   0;   0  -1   1   0   0;   0   0  -2   0   0;   0   0   0 
 B = [1;   0;   1;   0;   1];
 C = [1   0   0   0   0; 0   1   0   0   0; 0   0   0   1   0];
 D = [0;  0;  0];
-[am, bm, cm, dm] = minreal(A, B, C, D)
+[am, bm, cm, dm] = minreal(A, B, C, D);
 
 A_REF = [-2.2500   -0.5951   -0.0000;
    -0.4551   -1.7500   -1.1547;
-   -0.2425   -0.5774   -2.0000]
+   -0.2425   -0.5774   -2.0000];
 B_REF = [-0.0000; -0.0000; -1.7321];
 C_REF = [-0.0700    0.5000   -0.5774;
    -0.6301    0.5000         0;
@@ -70,20 +70,20 @@ D = [0.   0. ;  0.   0.];
 
 am_REF = [-1.0000    0.0000; 0.0000   -1.0000];
 bm_REF = [0.0418   -0.0350;  0.9143    1.0914];
-cm_REF = [-0.1482    1.0314; 0.4572    0.4841]
+cm_REF = [-0.1482    1.0314; 0.4572    0.4841];
 dm_REF = [0     0; 0     0];
 
 assert_isapprox(am, am_REF, 1e-4);
-assert_isapprox(bm, bm_REF, 1e-4);
-assert_isapprox(cm, cm_REF, 1e-4);
+assert_isapprox(abs(bm), abs(bm_REF), 1e-4);
+assert_isapprox(abs(cm), abs(cm_REF), 1e-4);
 assert_isapprox(dm, dm_REF, 1e-4);
 
-sysIn = ss(A, B, C, D)
+sysIn = ss(A, B, C, D);
 sysOut = minreal(sysIn);
 [am, bm, cm, dm] = ssdata(sysOut);
 
 assert_isapprox(am, am_REF, 1e-4);
-assert_isapprox(bm, bm_REF, 1e-4);
-assert_isapprox(cm, cm_REF, 1e-4);
+assert_isapprox(abs(bm), abs(bm_REF), 1e-4);
+assert_isapprox(abs(cm), abs(cm_REF), 1e-4);
 assert_isapprox(dm, dm_REF, 1e-4);
 %=============================================================================
