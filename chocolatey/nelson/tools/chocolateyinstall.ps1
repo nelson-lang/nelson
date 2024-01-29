af43fa95a70c81ddd1b88afce4de42b508b2304e
Allan CORNET <nelson.numerical.computation@gmail.com>
2024-01-29 21:49:44 +0100

    packaging chocolatey v1.1.0

--- chocolatey/nelson/tools/chocolateyinstall.ps1
+++ chocolatey/nelson/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$packageName = 'nelson'
 $installerType = 'exe'
 $silentArgs = '/VERYSILENT /ALLUSERS /SUPPRESSMSGBOXES /NORESTART /SP-'
-$url = 'https://github.com/nelson-lang/nelson/releases/download/v1.0.0/Nelson-1.0.0.3620-x86-32.exe'
-$checksum = '95b358a9634e4c241fe492103e826bbba92cd22f5423c863247c04dca4bb5ab8'
+$url = 'https://github.com/nelson-lang/nelson/releases/download/v1.1.0/Nelson-1.1.0.3654-x86-32.exe'
+$checksum = '11bfd714991964f6e32d6c72b572ee76cefcd47ac6661447a647df534b4dcfc4'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/nelson-lang/nelson/releases/download/v1.0.0/Nelson-1.0.0.3620-x86-64.exe'
-$checksum64 = '8b1dbd3ea023082a8ce8ce518db115ca108dfdd858e27d955260d60a60623829'
+$url64 = 'https://github.com/nelson-lang/nelson/releases/download/v1.1.0/Nelson-1.1.0.3654-x86-64.exe'
+$checksum64 = 'f37607a127365e7ec6e6344544f9c1a827960b92e8e03f225d62b3ff448eedbd'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0)
 
--- chocolatey/readme.md
+++ chocolatey/readme.md
@@ -8,8 +8,8 @@ https://community.chocolatey.org/account/Packages
 
 ```
 choco pack
-choco install nelson.v1.0.0.3620.nupkg -dv -s .
+choco install nelson.v1.1.0.3654.nupkg -dv -s .
 
 choco apikey -k <your key here> -s https://push.chocolatey.org/
-choco push nelson.1.0.0.3620.nupkg -s https://push.chocolatey.org/
+choco push nelson.1.1.0.3654.nupkg -s https://push.chocolatey.org/
 ```
