7c2a3c678703c3cb8a938b358ef54c67b61502c8
Allan CORNET <nelson.numerical.computation@gmail.com>
2024-03-07 06:45:23 +0100

    xcorr2 function (#1116)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - `contour` Contour plot of matrix.
 - `contour3` 3-D contour plot.
 - `shiftdim` Shift array dimensions.
+- `xcorr2` 2-D cross-correlation.
 
 ### Fixed
 
---
+++ modules/signal_processing/functions/xcorr2.m
@@ -0,0 +1,21 @@
+%=============================================================================
+% Copyright (c) 2016-present Allan CORNET (Nelson)
+%=============================================================================
+% This file is part of the Nelson.
+%=============================================================================
+% LICENCE_BLOCK_BEGIN
+% SPDX-License-Identifier: LGPL-3.0-or-later
+% LICENCE_BLOCK_END
+%=============================================================================
+function varargout = xcorr2(varargin)
+  narginchk(1, 2);
+  nargoutchk(0, 1);
+  A = varargin{1};
+  if nargin == 1
+    B = A;
+  else
+    B = varargin{2};
+  end
+  varargout{1} = conv2(A, rot90(conj(B), 2));
+end
+%=============================================================================
