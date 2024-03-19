68ab14620b066c56d4342d0342577b4da1600e2c
Allan CORNET <nelson.numerical.computation@gmail.com>
2024-03-19 09:31:40 +0100

    fix #1126 - isspace function

---
+++ modules/string/functions/isspace.m
@@ -0,0 +1,21 @@
+%=============================================================================
+% Copyright (c) 2016-present Allan CORNET (Nelson)
+%=============================================================================
+% This file is part of the Nelson.
+%=============================================================================
+% LICENCE_BLOCK_BEGIN
+% SPDX-License-Identifier: LGPL-3.0-or-later
+% LICENCE_BLOCK_END
+%=============================================================================
+function varargout = isspace(varargin)
+  narginchk(1, 1);
+  nargoutchk(0, 1);
+  A = varargin{1};
+  mustBeA(A, ["char", "string"], 1);
+  if isstring(A)
+    mustBeTextScalar(A, 1);
+  end
+  A = convertStringsToChars(A);
+  varargout{1} = (A == ' ');
+end
+%=============================================================================
