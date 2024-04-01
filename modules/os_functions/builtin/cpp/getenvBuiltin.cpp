4605addaa0248af1531d142604ee06f167b5f41b
Allan CORNET <nelson.numerical.computation@gmail.com>
2024-04-01 08:50:57 +0200

    getenv : Retrieve the values of several environment variables (#1151)

--- modules/os_functions/builtin/cpp/getenvBuiltin.cpp
+++ modules/os_functions/builtin/cpp/getenvBuiltin.cpp
@@ -21,12 +21,44 @@ Nelson::OsFunctionsGateway::getenvBuiltin(int nLhs, const ArrayOfVector& argIn)
     nargincheck(argIn, 1, 1);
     nargoutcheck(nLhs, 0, 1);
     ArrayOfVector retval;
-    if (argIn[0].isRowVectorCharacterArray()) {
-        std::wstring varEnvName = argIn[0].getContentAsWideString();
-        std::wstring ret = GetVariableEnvironment(varEnvName);
-        retval << ArrayOf::characterArrayConstructor(ret);
-    } else {
+    NelsonType currentClass = argIn[0].getDataClass();
+    switch (currentClass) {
+    case NLS_CHAR: {
+        if (argIn[0].isRowVectorCharacterArray()) {
+            std::wstring varEnvName = argIn[0].getContentAsWideString();
+            std::wstring ret = GetVariableEnvironment(varEnvName);
+            retval << ArrayOf::characterArrayConstructor(ret);
+        } else {
+            Error(ERROR_WRONG_ARGUMENT_1_TYPE_STRING_EXPECTED);
+        }
+    } break;
+    case NLS_CELL_ARRAY:
+    case NLS_STRING_ARRAY: {
+        if (argIn[0].isScalarStringArray()) {
+            std::wstring varEnvName = argIn[0].getContentAsWideString();
+            std::wstring ret = GetVariableEnvironment(varEnvName);
+            retval << ArrayOf::characterArrayConstructor(ret);
+            return retval;
+        }
+        ArrayOf* elements = (ArrayOf*)argIn[0].getDataPointer();
+        ArrayOf* outputs
+            = (ArrayOf*)ArrayOf::allocateArrayOf(currentClass, argIn[0].getElementCount());
+        ArrayOf res = ArrayOf(currentClass, argIn[0].getDimensions(), outputs);
+        for (size_t k = 0; k < argIn[0].getElementCount(); ++k) {
+            if (elements[k].isRowVectorCharacterArray()) {
+                std::wstring varEnvName = elements[k].getContentAsWideString();
+                std::wstring ret = GetVariableEnvironment(varEnvName);
+                outputs[k] = ArrayOf::characterArrayConstructor(ret);
+            } else {
+                Error(_W("Environment names must be a string array, cell array of character "
+                         "vectors, or character array."));
+            }
+        }
+        retval << res;
+    } break;
+    default: {
         Error(ERROR_WRONG_ARGUMENT_1_TYPE_STRING_EXPECTED);
+    } break;
     }
     return retval;
 }
