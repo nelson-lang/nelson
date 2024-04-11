7bd5a89150d9f97eabefb5d7fdb4baca49855619
Allan CORNET <nelson.numerical.computation@gmail.com>
2024-04-11 06:45:46 +0200

    fix #1148 - pyrun('print(A)','A','A',string(NaN)) did not return expected value. (#1158)

---
+++ modules/python_engine/tests/bug_github_issue_#1148.m
@@ -0,0 +1,18 @@
+%=============================================================================
+% Copyright (c) 2017 Allan CORNET (Nelson)
+%=============================================================================
+% This file is part of the Nelson.
+%=============================================================================
+% LICENCE_BLOCK_BEGIN
+% SPDX-License-Identifier: LGPL-3.0-or-later
+% LICENCE_BLOCK_END
+%=============================================================================
+% <-- Issue URL -->
+% https://github.com/nelson-lang/nelson/issues/1148
+% <-- Short Description -->
+% pyrun('print(A)','A','A',string(NaN)) did not return expected value.
+%=============================================================================
+R = pyrun('print(A)','A','A',string(NaN));
+assert_isequal(class(R), 'py.NoneType')
+assert_isequal(R.char, 'None')
+%=============================================================================
