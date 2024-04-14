1921fb6efb0d37948bbf4491f5779fad79ca3836
Allan CORNET <nelson.numerical.computation@gmail.com>
2024-04-14 09:29:25 +0200

    Fix #1157 - Qt 6.7 support

--- modules/qml_engine/tests/test_QObject_isprop.m
+++ modules/qml_engine/tests/test_QObject_isprop.m
@@ -21,9 +21,14 @@ end
 qobj = qml_loadfile(qml_file_ok);
 assert_istrue(isprop(qobj, 'x'));
 assert_istrue(isprop(qobj, 'y'));
-assert_isfalse(isprop(qobj, 'z'));
-qobj.z = 1;
-assert_istrue(isprop(qobj, 'z'));
+if (semver(qt_version(), '6.7.0') < 0)
+  assert_isfalse(isprop(qobj, 'z'));
+  qobj.z = 1;
+  assert_istrue(isprop(qobj, 'z'));
+else
+  assert_istrue(isprop(qobj, 'z'));
+  assert_isequal(qobj.z, 0);
+end
 %=============================================================================
 delete(qobj);
 %=============================================================================
