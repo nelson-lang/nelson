fc0139a096829cea59aaaef179b3764b68cd098c
Allan CORNET <nelson.numerical.computation@gmail.com>
2024-10-24 20:27:45 +0200

    Fix #1248: add renamevars and removevars functions
    - Implements renamevars() to rename table variables
    - Implements removevars() to remove table variables
    - Updates subsasgn and subsref table methods
    - Adds documentation and tests

---
+++ modules/table/functions/removevars.m
@@ -0,0 +1,23 @@
+%=============================================================================
+% Copyright (c) 2016-present Allan CORNET (Nelson)
+%=============================================================================
+% This file is part of the Nelson.
+%=============================================================================
+% LICENCE_BLOCK_BEGIN
+% SPDX-License-Identifier: LGPL-3.0-or-later
+% LICENCE_BLOCK_END
+%=============================================================================
+function varargout = removevars(varargin)
+  nargoutchk(0, 1);
+  narginchk(1, 2);
+  T = varargin{1};
+  mustBeA(T, 'table', 1);
+  if (nargin < 2)
+    vars = [];
+  else
+    vars = varargin{2};
+  end
+  T(:, vars) = [];
+  varargout{1} = T;
+end
+%=============================================================================
