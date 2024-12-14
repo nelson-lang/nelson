94ae26b3df94f7a03704175adbef1a5d69d3ac12
Allan CORNET <nelson.numerical.computation@gmail.com>
2024-12-14 22:08:27 +0100

    chocolatey packaging

--- chocolatey/nelson/tools/chocolateyinstall.ps1
+++ chocolatey/nelson/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageName = 'nelson'
 $installerType = 'exe'
 $silentArgs = '/VERYSILENT /ALLUSERS /SUPPRESSMSGBOXES /NORESTART /SP-'
-$url = 'https://github.com/nelson-lang/nelson/releases/download/v1.10/Nelson-1.10.4516-x86-32.exe'
+$url = 'https://github.com/nelson-lang/nelson/releases/download/v1.10.0/Nelson-1.10.0.4516-x86-32.exe'
 $checksum = 'c94fc765b8363efe5ee04730ae010109607fc25e5e4a375a525e43eb97734aa4'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/nelson-lang/nelson/releases/download/v1.10/Nelson-1.10.4516-x86-64.exe'
+$url64 = 'https://github.com/nelson-lang/nelson/releases/download/v1.10.0/Nelson-1.10.0.4516-x86-64.exe'
 $checksum64 = 'cd3d1ea3b9a4e923397ccb75c0652f7ca0f44f967395df1266eef9b7804a4819'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0)
