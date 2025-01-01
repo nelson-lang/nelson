7b9108609dbb3bf177df0ec6752ebf6793855245
Allan CORNET <nelson.numerical.computation@gmail.com>
2025-01-01 17:43:05 +0100

    chore(NixOS) remove pkgs refs and use Qt6 (#1318)

--- nelson.nix
+++ nelson.nix
@@ -1,5 +1,9 @@
 {
-  pkgs,
+  stdenv, # Standard environment providing basic build tools and utilities
+  lib,  # Nix standard library with helper functions and utilities
+  fetchFromGitHub,  # Function to fetch source code from GitHub repositories
+  cmake, # Build system generator
+  pkg-config, # Package configuration tool
   zlib, # Compression library
   boost, # Boost libraries
   openssl, # SSL support
@@ -18,48 +22,48 @@
   alsa-lib, # ALSA support
   matio, # MATIO support
   libcxx, # C++ standard library
-  qt5, # Qt for GUI support
+  qt6, # Qt for GUI support
 }:
 
-pkgs.stdenv.mkDerivation {
+stdenv.mkDerivation {
   buildInputs = [
-    pkgs.zlib # Compression library
-    pkgs.boost # Boost libraries
-    pkgs.openssl # SSL support
-    pkgs.mpi # MPI support
-    pkgs.hdf5 # HDF5 support
-    pkgs.curl # cURL support
-    pkgs.libgit2 # Git support
-    pkgs.libsndfile # Sound file support
-    pkgs.eigen # Eigen library
-    pkgs.portaudio # PortAudio support
-    pkgs.lapack # LAPACK support
-    pkgs.openblas # OpenBLAS support
-    pkgs.libjack2 # JACK support
-    pkgs.taglib # TagLib support
-    pkgs.alsa-oss # ALSA support
-    pkgs.alsa-lib # ALSA support
-    pkgs.matio # MATIO support
-    pkgs.libcxx # C++ standard library
-    pkgs.qt5.full # Qt for GUI support
+    zlib # Compression library
+    boost # Boost libraries
+    openssl # SSL support
+    mpi # MPI support
+    hdf5 # HDF5 support
+    curl # cURL support
+    libgit2 # Git support
+    libsndfile # Sound file support
+    eigen # Eigen library
+    portaudio # PortAudio support
+    lapack # LAPACK support
+    openblas # OpenBLAS support
+    libjack2 # JACK support
+    taglib # TagLib support
+    alsa-oss # ALSA support
+    alsa-lib # ALSA support
+    matio # MATIO support
+    libcxx # C++ standard library
+    qt6.full # Qt for GUI support
+    qt6.qtbase # Qt base required
   ];
 
   nativeBuildInputs = [
-    pkgs.cmake # Build system
-    pkgs.gfortran # Fortran compiler
-    pkgs.pkg-config # pkg-config for detecting dependencies
-    pkgs.qt5.wrapQtAppsHook # Wrapper for Qt applications
+    cmake # Build system
+    pkg-config # pkg-config for detecting dependencies
+    qt6.wrapQtAppsHook # Wrapper for Qt applications
   ];
 
   name = "nelson";
   version = "master";
 
   # Source URL for the Nelson source code
-  src = pkgs.fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "nelson-lang";
     repo = "nelson";
     rev = "master";
-    sha256 = "sha256-b7L/ahq39eYDvFLW2BS9kYjS2eR2c+yPsMp0mBBJqLU="; # Replace with the actual checksum
+    sha256 = "sha256-9UYEL57yJeRR1lLD+o3R9cPKYLTqcPxwIAv8pf3Rf6Y="; # Replace with the actual checksum
   };
 
   cmakeFlags = [
@@ -79,8 +83,8 @@ pkgs.stdenv.mkDerivation {
   meta = {
     description = "Nelson programming language";
     homepage = "https://github.com/nelson-lang/nelson";
-    license = pkgs.lib.licenses.lgpl3Plus;
-    maintainers = with pkgs.lib.maintainers; [ ];
+    license = lib.licenses.lgpl3Plus;
+    maintainers = with lib.maintainers; [ ];
     mainProgram = "nelson";
   };
 
