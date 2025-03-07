0b1eca20a1b455bf5b09876bd281163c553aa444
Allan CORNET <nelson.numerical.computation@gmail.com>
2025-03-07 08:36:15 +0100

    chore(chocolatey): remove win32 packaging

--- chocolatey/nelson/tools/chocolateyinstall.ps1
+++ chocolatey/nelson/tools/chocolateyinstall.ps1
@@ -1,9 +1,6 @@
 ﻿$packageName = 'nelson'
 $installerType = 'exe'
 $silentArgs = '/VERYSILENT /ALLUSERS /SUPPRESSMSGBOXES /NORESTART /SP-'
-$url = 'https://github.com/nelson-lang/nelson/releases/download/v1.12.0/Nelson-1.12.0.4690-x86-32.exe'
-$checksum = '957aff1be6055253745057496160af3081a4f05c75c73cfdb9348ddb1d127863'
-$checksumType = 'sha256'
 $url64 = 'https://github.com/nelson-lang/nelson/releases/download/v1.12.0/Nelson-1.12.0.4690-x86-64.exe'
 $checksum64 = '6777ebd959ddd62ab91494e49fe3195934a8c98e218b386afb82609ce667b4fe'
 $checksumType64 = 'sha256'
@@ -12,10 +9,7 @@ $validExitCodes = @(0)
 Install-ChocolateyPackage -PackageName "$packageName" `
                           -FileType "$installerType" `
                           -SilentArgs "$silentArgs" `
-                          -Url "$url" `
                           -Url64bit "$url64" `
                           -ValidExitCodes $validExitCodes `
-                          -Checksum "$checksum" `
-                          -ChecksumType "$checksumType" `
                           -Checksum64 "$checksum64" `
                           -ChecksumType64 "$checksumType64"
