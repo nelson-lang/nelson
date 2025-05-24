018656391eb5611455b0f4424de87922033c7abb
Allan CORNET <nelson.numerical.computation@gmail.com>
2025-05-24 11:45:14 +0200

    chore: fix application ID (#1411)

--- modules/core/tests/test_nelsonappid.m
+++ modules/core/tests/test_nelsonappid.m
@@ -7,5 +7,5 @@
 % SPDX-License-Identifier: LGPL-3.0-or-later
 % LICENCE_BLOCK_END
 %=============================================================================
-assert_isequal(nelsonappid(), 'io.github.nelson-lang.Nelson');
+assert_isequal(nelsonappid(), 'io.github.nelson_lang.Nelson');
 %=============================================================================
