19fa95176c13bc65d78408548ceab1dc5e60c989
Allan CORNET <nelson.numerical.computation@gmail.com>
2025-06-22 21:26:05 +0200

    fix(core): ensure 'ans' is correctly created only for expressions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,6 +16,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - i18nExtractor reworked (faster and modernized).
 - Browser variable reworked.
 
+### Fixed
+
+- ensure 'ans' is correctly created only for expressions.
+
 ## 1.14.0 - (2025-05-30)
 
 ### Added
---
+++ modules/interpreter/tests/test_ans.m
@@ -0,0 +1,19 @@
+%=============================================================================
+% Copyright (c) 2016-present Allan CORNET (Nelson)
+%=============================================================================
+% This file is part of the Nelson.
+%=============================================================================
+% LICENCE_BLOCK_BEGIN
+% SPDX-License-Identifier: LGPL-3.0-or-later
+% LICENCE_BLOCK_END
+%=============================================================================
+clear ans
+assert_isfalse(isvar('ans'))
+B = 33;
+B
+assert_isfalse(isvar('ans'))
+B = {33};
+B
+assert_isfalse(isvar('ans'))
+33
+assert_istrue(isvar('ans'))
