2312b1c231dd3be8fa5dec50af8d51694c3258e6
Allan CORNET <nelson.numerical.computation@gmail.com>
2025-09-09 19:16:19 +0200

    fix(chocolatey): error code allowed

--- chocolatey/nelson/tools/chocolateyinstall.ps1
+++ chocolatey/nelson/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $silentArgs = '/VERYSILENT /ALLUSERS /SUPPRESSMSGBOXES /NORESTART /SP-'
 $url64 = 'https://github.com/nelson-lang/nelson/releases/download/v1.14.0/Nelson-1.14.0.5060-x86-64.exe'
 $checksum64 = 'e2d712798646ea2b1aae8a2dc74b3cd8523644f3242eb756f73ee76b502ed7bb'
 $checksumType64 = 'sha256'
-$validExitCodes = @(0)
+$validExitCodes = @(0, 1638, 3010)
 
 Install-ChocolateyPackage -PackageName "$packageName" `
                           -FileType "$installerType" `
