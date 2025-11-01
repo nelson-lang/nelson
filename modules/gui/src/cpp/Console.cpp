439cd56e73075d01c935c13d4c7aca1bba850264
Allan CORNET <nelson.numerical.computation@gmail.com>
2025-11-01 12:38:48 +0100

    chore: prettier

--- modules/gui/src/cpp/Console.cpp
+++ modules/gui/src/cpp/Console.cpp
@@ -69,7 +69,8 @@ CreateConsole()
     if (hNelSonConsole) {
         ShowWindow(hNelSonConsole, SW_HIDE);
 
-        // Disable the Close (X) button on the console window to prevent closing via the cross button
+        // Disable the Close (X) button on the console window to prevent closing via the cross
+        // button
         HMENU hMenu = GetSystemMenu(hNelSonConsole, FALSE);
         if (hMenu) {
             // Gray out the Close menu item
