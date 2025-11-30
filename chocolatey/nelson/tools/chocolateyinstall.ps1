a6bfb74b07a205d460bce1e5b07c8381d62652d7
Allan CORNET <nelson.numerical.computation@gmail.com>
2025-11-30 20:47:40 +0100

    fix: sha256 packaging

--- chocolatey/nelson/tools/chocolateyinstall.ps1
+++ chocolatey/nelson/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $installerType = 'exe'
 $silentArgs = '/VERYSILENT /ALLUSERS /SUPPRESSMSGBOXES /NORESTART /SP-'
 $url64 = 'https://github.com/nelson-lang/nelson/releases/download/v1.15.0/Nelson-1.15.0.5475-x86-64.exe'
-$checksum64 = 'sha256:eacd4a9109094678c43a4c9de91d185b1357ef8ed0e8f49c4ac7849c0b608e99'
+$checksum64 = 'eacd4a9109094678c43a4c9de91d185b1357ef8ed0e8f49c4ac7849c0b608e99'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0, 1638, 3010)
 
